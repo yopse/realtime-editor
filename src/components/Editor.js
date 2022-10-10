91126403065939d2634eb6d8e11cca0ae4c98842
yopse <82827725+yopse@users.noreply.github.com>
2022-10-11 02:52:19 +0530

    unused imports removed

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import Codemirror, { commands } from "codemirror";
+import Codemirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/dracula.css";
 import "codemirror/mode/javascript/javascript";
